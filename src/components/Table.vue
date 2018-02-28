5c1ee820618f9d84eb3a24f5bf67224953c50660
louiemain <louielcxy@live.cn>
2018-02-28 20:46:41 +0800

    add table data binding from springboot server.

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -7,7 +7,7 @@
         <BreadcrumbItem to="/table">表格1</BreadcrumbItem>
       </Breadcrumb>
       <Content :style="{background: '#fff', minHeight: '70vh'}">
-        <Table border :columns="columns" :data="data"></Table>
+        <Table border :columns="columns" :data="data1"></Table>
       </Content>
     </Layout>
   </div>
@@ -15,29 +15,34 @@
 <script>
   export default {
     data() {
-      this.$http.get('/item/all').then(response => {
-        var da = response.data.data;
-      });
       return {
-        columns: [
-          {
-            title: 'Name',
-            key: 'name'
-          },
-          {
-            title: 'Age',
-            key: 'age'
-          },
-          {
-            title: 'Address',
-            key: 'address'
-          }
-        ],
-        data: da
+        columns: [],
+        data1: []
       }
     },
+    mounted(){
+      this.$http.get('/tab/all').then(response => {
+      var da = response.data;
+      var columns = [
+        {
+          title: 'Name',
+          key: 'name'
+        },
+        {
+          title: 'Age',
+          key: 'age'
+        },
+        {
+          title: 'Address',
+          key: 'address'
+        }
+      ];
+      this.columns = columns;
+      this.data1 = JSON.parse(JSON.stringify(da));
+    });
+    },
     created() {
-
+      // this.init();
     },
     methods: {
 
